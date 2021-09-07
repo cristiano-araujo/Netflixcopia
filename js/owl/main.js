e21ac234bfa1b446047117fc98a1441789f838f7
Cristiano112715 <cristiano112715@gmail.com>
2021-09-06 22:18:43 -0300

    commit inicial

---
+++ js/owl/main.js
@@ -0,0 +1,8 @@
+function redirecionar() {
+    window.open("https://www.youtube.com/watch?v=rIx91BKV9mo");
+}
+
+function redirecionar1() {
+    window.open("https://wp.ufpel.edu.br/empauta/paulo-apostolo-de-cristo/");
+}
+
